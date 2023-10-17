d9afa69d259e849a93d08cc7e8ba5ee5fb8b410a
Valery <valera.vlasov11@mail.ru>
2023-10-17 22:02:49 +0300

    Добавьте файлы проекта.

---
+++ Snake/Images.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Snake
+{
+    public static class Images
+    {
+        //public readonly static ImageSource Empty = LoadImage("Empty.png");
+        //public readonly static ImageSource Empty = LoadImage("Empty.png");
+        public readonly static ImageSource SnakeHead = LoadImage("SnakeHead.png");
+        public readonly static ImageSource SnakeBody = LoadImage("SnakeBody.png");
+        public readonly static ImageSource SnakeLogo = LoadImage("Snake.png");
+        public readonly static ImageSource Food = LoadImage("FoodApple.png");
+        private static ImageSource LoadImage(string fileName)
+        {
+            return new BitmapImage(new Uri($"Assets/{fileName}", UriKind.Relative));
+        }
+    }
+}
